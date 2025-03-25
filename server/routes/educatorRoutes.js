992b497c4fcceceaf99b74994a786e44105d6f9d
Maretha kunal <kunalmaretha2003@gmail.com>
2025-03-25 18:02:00 +0530

    fix: remove educator controller and routes as part of refactoring

--- server/routes/educatorRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {upRoleToEduactor} from '../controllers/educatorController.js';
-
-const router = express.Router();
-
-router.get('/update-role',upRoleToEduactor);
-
-export default router;
