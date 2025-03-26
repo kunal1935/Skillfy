e65208ef5835f4246eca7b39b21260f82f51d7c3
Maretha kunal <kunalmaretha2003@gmail.com>
2025-03-26 08:50:41 +0530

    ``` fix: update educator role handling in routes ```

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,7 +1,7 @@
 import { clerkClient } from "@clerk/express"
 
 
-export const updateRoletoEducator = async ()=>{
+export const updateRoletoEducator = async ( req , res)=>{
     try{
         const userId =req.auth.userId
         await clerkClient.users.updateUserMetadata(userId, {
